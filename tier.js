54378007293143d935a7d5966282219484ea9901
hanfengyds <2174086642@qq.com>
2025-08-14 14:53:18 +0800

    Update tier.js

--- tier.js
+++ tier.js
@@ -8,10 +8,10 @@ const tierPokemons = {
         'pecharunt', 'primarina', 'raging-bolt', 'samurott-hisui', 'slowking-galar',
         'ting-lu', 'tinkaton', 'walking-wake', 'weavile', 'zamazenta', 'zapdos'
     ],
-    uubl: [
-        'blaziken', 'ceruledge', 'garchomp', 'hoopa-unbound', 'iron-boulder',
-                    'iron-hands', 'latias', 'meowscarada', 'moltres-galar', 'okidogi',
-                    'pelipper', 'polteageist', 'quaquaval', 'rillaboom', 'ursaluna'
+    uubl: [ //15个
+         'blaziken', 'garchomp','hoopa-unbound','iron-boulder','iron-hands',
+         'kommo-o','latias','meowscarada','moltres-galar','okidogi',
+         'pelipper','polteageist','quaquaval','rillaboom','ursaluna',
     ],
     uu: [
         'arcanine-hisui', 'azumarill', 'blissey', 'clodsire', 'cobalion',
@@ -43,7 +43,7 @@ const tierPokemons = {
         'volcarona',
         'zacian', 'zacian-crowned', 'zamazenta-crowned', 'zekrom'
     ],
-    第一赛段: [
+    第一赛段: [ //30
        'gigalith', 'sandslash', 'poliwrath', 'lanturn',
                     'pelipper', 'escavalier', 'amoonguss', 'chesnaught', 'ampharos-Mega',
                     'crawdaunt', 'lucario', 'flygon', 'articuno',
@@ -52,7 +52,7 @@ const tierPokemons = {
 
     ],
 
-    第二赛段: [
+    第二赛段: [ //30
         'keldeo-ordinary', 'aegislash-shield', 'sableye-mega', 'corviknight', 'volcanion', 
         'jellicent', 'tornadus-therian', 'slowking-galar', 'greninja', 'iron-crown', 
         'tapu-fini', 'articuno', 'terrakion', 'manectric-mega', 'golisopod', 'sandy-shocks', 'tangrowth',
@@ -60,11 +60,25 @@ const tierPokemons = {
         ,'clodsire','bewear','nihilego','buzzwole',
         'salamence','infernape'
     ],
-    RU残奥会:[
+    RU残奥会:[ //30
       'incineroar','primarina','krookodile','abomasnow-mega','cryogonal','toedscruel',
       'ninetales-alola','zoroark-hisui','decidueye-hisui','dhelmise','crobat','weezing-galar',
       'heracross-mega','steelix-mega','electrode-hisui','electivire','magmortar','wyrdeer','mimikyu-totem-disguised',
       'golem-alola','slowking','durant','tentacruel','quagsire','bruxish',
       'bronzong','sneasel','flygon','jellicent','mandibuzz'
-    ]
+    ],
+
+    第三赛段:[
+      'absol-mega','venusaur-mega',//灾兽，妙蛙花
+      'gyarados','rotom-wash','celesteela','latios',//暴鲤龙，洗衣机，铁火辉夜，拉帝欧斯
+      'clefable','cobalion','golisopod','garchomp',//皮可西，勾帕路翁，具甲武者，烈咬陆鲨
+      'landorus-therian','zeraora','ceruledge','raging-bolt',//土地云，捷拉奥拉，苍炎刃鬼，猛雷鼓
+      'iron-valiant','gholdengo','magearna','moltres','iron-treads',//铁舞者，赛富豪，玛机雅娜，火焰鸟，铁辙迹
+      'terapagos-terastal','toxapex','iron-boulder','kartana','heatran',//泰勒巴格斯，超坏星，铁磐岩，纸御剑，席多蓝恩
+      'dialga','hawlucha','ogerpon-cornerstone-mask','houndoom-mega','hippowdon',//帝牙卢卡，摔角鹰人，岩砰砰，黑鲁加，河马兽
+      'electivire',//电击魔兽
+
+
+    ],
+
 };
